0cf6ff1b3acd7860844aba76978a8c4c164286be
Kevin Tian <ktian0257@gmail.com>
2019-01-04 15:52:10 -0800

    New app entry point
    New entry point for application in the form of a javascript class,
    this way any initialization needed for can be done clearly.

--- main.js
+++ main.js
@@ -1,47 +1,82 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow} = require('electron');
+const log = require("electron-log");
 
 // global reference to window so we can keep in memory until we want to close it
 let win
 
-function initialize() {
-    // window constants
+/** 
+ * Encloses the application life cycle in a class 
+ */
+class App {
+    /**
+     * @constructor
+     */
+    constructor() {
+        // initialize all required class/function dependencies here
 
-    // Create Initial window
+    }
+
+    // functions go below
+    /**
+     * main event listeners for each stage of app lifecycle
+     */
+    eventListeners() {
+        app.on('ready', this.readyEventHandler.bind(this));
+        app.on('window-all-closed', this.allclosedEventHandler.bind(this));
+        app.on('activate', this.activateEventHandler.bind(this));
+    }
+
+    /**
+     * prepares and displays main window
+     */
+    readyEventHandler() {
+        // temp function
+        createWindow();
+    }
+
+    /**
+     * quits application when all windows are closed
+     */
+    allclosedEventHandler() {
+        if (process.platform !== 'darwin') {
+            app.quit();
+        }
+    }
+
+    /**
+     * for loading from dock or tray
+     */
+    activateEventHandler() {
+        if (win !== null) {
+            createWindow();
+        }
+    }
+}
+
+/* App starts go below */
+// start app
+const appInst = new App();
+appInst.eventListeners();
+
+    /**
+     * temp create main window function, to be replaced by window preparer
+     */
     function createWindow() {
         // create browser window
-        win = new BrowserWindow({ width: 800, height: 600 })
+        win = new BrowserWindow({ width: 800, height: 600 });
     
         // load index html as main page
-        win.loadFile('index.html')
+        win.loadFile('index.html');
         
         // dev tools
-        win.webContents.openDevTools()
+        win.webContents.openDevTools();
 
         // window closed event
         win.on('closed', () => {
             // window is removed from memory, if app has multiple windows,
             // use window array and remove that particular element.
-            win = null
+            win = null;
         })
     }
 
-    // ran after all electron initialization has finished
-    app.on('ready', createWindow)
-    
-    // all windows closed event
-    app.on('window-all-closed', () => {
-        // conditional for macOS
-        if (process.platform !== 'darwin') {
-            app.quit()
-        }
-    })
-
-    // re-activating window from either tray or macOS dock
-    app.on('activate', () => {
-        if (win === null) {
-            createWindow()
-        }
-    })
-}
-
-initialize()
+// testing suites go below
